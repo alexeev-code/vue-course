97a96c5ae53689da5c24c54a4a4e4add3cd54d87
Your Name <you@example.com>
2025-04-18 01:46:14 +0700

    new code

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Счётчик'
+            title: 'Note list',
+            placeholderString: 'text...',
+            inputValue: '',
+            noteList: []
+        }
+    },
+    methods: {
+        inputData(event) {
+            this.inputValue = event.target.value
+        },
+        addNote() {
+            if (this.inputValue !== '') {
+                this.noteList.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        doubleCount(){
+            console.log('TUT!!!')
+            return this.noteList.length * 2
+        },
+        toUpperCase(item){
+            return item.toUpperCase()
+        },
+        deleteNote(i) {
+            this.noteList.splice(i, 1)
         }
     }
 }
